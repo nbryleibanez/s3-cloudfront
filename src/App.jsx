f75935ef4f3b61cb0bb49efa072d363cd99a45e5
Nhyl Bryle Ibanez <nbryleibanez@gmail.com>
2025-02-18 21:49:01 +0800

    feat: new feature

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>ALC Study Session</h1>
+      <h1>ALC Study Session #2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
