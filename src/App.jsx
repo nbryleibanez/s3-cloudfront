806d05b27d698ba04c27611b53f540f0c350a8b5
Nhyl Bryle Ibanez <nbryleibanez@gmail.com>
2025-02-18 22:00:35 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
